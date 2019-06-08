9ad13c38c0d1b1e87e0654bb6b65c5d6b9efe266
AgusQuintanar <agusquintanar17@gmail.com>
2019-06-08 11:34:26 -0500

    Added ignored statements

--- Data Structures and Algorithms/1.2_Unpacking_Elements_from Iterables_of_Arbitrary_Length.py
+++ Data Structures and Algorithms/1.2_Unpacking_Elements_from Iterables_of_Arbitrary_Length.py
@@ -4,3 +4,11 @@ columna, *datos = items
 
 print(columna)
 print(datos)
+
+        #  name       last name    current semester  age   expected graduation date
+student = ['agustin','quintanar', 'second semester', '19', (10,12,2022)]
+
+name, *ignored, (*ignored ,expected_graduation_year) = student
+
+print('Student name:',name)
+print('Expected graduation year:',expected_graduation_year)
